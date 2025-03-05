a7464a2b8fcd39d38523236c0d77369a8b95fb35
Alex Uribe <repzyyog@gmail.com>
2025-03-05 18:29:12 +0100

    Add tests to getShortWordsList function

---
+++ src/analysis/__tests__/getShortWordsList.ts
@@ -0,0 +1,79 @@
+import { getShortWordsList } from "../index.js";
+
+export const getShortWordsListTests = (): void => {
+  console.log("\n\n➡️  Test: getShortWordsList\n");
+  test1();
+  test2();
+  test3();
+  test4();
+};
+
+export const test1 = (): void => {
+  const text: string[] = ["Hola"];
+  const expectedShortWordsList = "Hola";
+  const actualShortWordsList = getShortWordsList(text);
+
+  console.log(`Test 1: Input: [${text}]`);
+  console.log(
+    `Expected Short Words List: "${expectedShortWordsList}" | Result: "${actualShortWordsList}"`
+  );
+
+  if (actualShortWordsList === expectedShortWordsList) {
+    console.log("Test ✅\n");
+  } else {
+    console.log("Text ❌\n");
+  }
+};
+
+export const test2 = (): void => {
+  const text: string[] = ["Hola", "Adios", "Bonjourno"];
+  const expectedShortWordsList = "Hola, Adios, Bonjourno";
+  const actualShortWordsList = getShortWordsList(text);
+
+  console.log(`Test 2: Input: [${text}]`);
+  console.log(
+    `Expected Short Words List: "${expectedShortWordsList}" | Result: "${actualShortWordsList}"`
+  );
+
+  if (actualShortWordsList === expectedShortWordsList) {
+    console.log("Test ✅\n");
+  } else {
+    console.log("Text ❌\n");
+  }
+};
+
+export const test3 = (): void => {
+  const text: string[] = [];
+  const expectedShortWordsList = "";
+  const actualShortWordsList = getShortWordsList(text);
+
+  console.log(`Test 3: Input: [${text}]`);
+  console.log(
+    `Expected Short Words List: "${expectedShortWordsList}" | Result: "${actualShortWordsList}"`
+  );
+
+  if (actualShortWordsList === expectedShortWordsList) {
+    console.log("Test ✅\n");
+  } else {
+    console.log("Text ❌\n");
+  }
+};
+
+export const test4 = (): void => {
+  const text: string[] = [""];
+  const expectedShortWordsList = "";
+  const actualShortWordsList = getShortWordsList(text);
+
+  console.log(`Test 4: Input: [${text}]`);
+  console.log(
+    `Expected Short Words List: "${expectedShortWordsList}" | Result: "${actualShortWordsList}"`
+  );
+
+  if (actualShortWordsList === expectedShortWordsList) {
+    console.log("Test ✅\n");
+  } else {
+    console.log("Text ❌\n");
+  }
+};
+
+//console.log(["", " ", ""]); // ""
--- src/analysis/__tests__/index.ts
+++ src/analysis/__tests__/index.ts
@@ -2,8 +2,10 @@ import { getParagraphsTotalTests } from "./getParagraphsTotal.js";
 import { getWordsTotalTests } from "./getWordsTotal.js";
 import { getCharactersTotalTests } from "./getCharactersTotal.js";
 import { getShortWordsTotalTests } from "./getShortWordsTotal.js";
+import { getShortWordsListTests } from "./getShortWordsList.js";
 
 getParagraphsTotalTests();
 getWordsTotalTests();
 getCharactersTotalTests();
 getShortWordsTotalTests();
+getShortWordsListTests();
