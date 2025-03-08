26974a2cfa6a7a7a988a16c04c995349ba2790bd
Alex Uribe <repzyyog@gmail.com>
2025-03-08 13:32:33 +0100

    Delete analysis blocks not used

--- src/ui/analysis.ts
+++ src/ui/analysis.ts
@@ -12,28 +12,20 @@ import {
 
 const totalsContainer = document.querySelector(".totals");
 const shortWordsContainer = document.querySelector(".short");
-const palindromesContainer = document.querySelector(".palindromes");
 const frequenciesContainer = document.querySelector(
   ".frequencies .frequencies-list"
 );
 const reversedWordsContainer = document.querySelector(".reversed-words");
-const reversedTextContainer = document.querySelector(".reversed-text");
 const forbiddenContainer = document.querySelector(".forbidden");
 const camelCaseContainer = document.querySelector(".camel-case");
-const kebabCaseContainer = document.querySelector(".kebab-case");
-const snakeCaseContainer = document.querySelector(".snake-case");
 
 if (
   !totalsContainer ||
   !shortWordsContainer ||
-  !palindromesContainer ||
   !frequenciesContainer ||
   !reversedWordsContainer ||
-  !reversedTextContainer ||
   !forbiddenContainer ||
-  !camelCaseContainer ||
-  !kebabCaseContainer ||
-  !snakeCaseContainer
+  !camelCaseContainer
 ) {
   throw new Error("Missing elements");
 }
@@ -62,10 +54,6 @@ const renderShortWordsList = (shortWords: string): void => {
   shortWordsContainer.querySelector(".short-words")!.textContent = shortWords;
 };
 
-const renderPalindromes = (palindromes: string): void => {
-  palindromesContainer.querySelector(".palindromes")!.textContent = palindromes;
-};
-
 const renderForbiddenWords = (forbiddenWords: string): void => {
   forbiddenContainer.querySelector(".censured-text")!.textContent =
     forbiddenWords;
@@ -76,36 +64,19 @@ const renderReversedWords = (reversedWords: string): void => {
     reversedWords;
 };
 
-const renderReversedText = (reversedText: string): void => {
-  reversedTextContainer.querySelector(".reversed-text-list")!.textContent =
-    reversedText;
-};
-
 const renderCamelCase = (camelCaseText: string): void => {
   camelCaseContainer.querySelector(".camel-case-text")!.textContent =
     camelCaseText;
-};
-
-const renderKebabCase = (kebabCaseText: string): void => {
-  kebabCaseContainer.querySelector(".kebab-case-text")!.textContent =
-    kebabCaseText;
-};
-
-const renderSnakeCase = (snakeCaseText: string): void => {
-  snakeCaseContainer.querySelector(".snake-case-text")!.textContent =
-    snakeCaseText;
 };
 
 const listenForbiddenWordsChanges = (text: string): void => {
   forbiddenContainer
     .querySelector("input")!
     .addEventListener("input", (event) => {
-      // The variable forbiddenWords contains the words entered by the user in the input, already splitted
       const forbiddenWords = (event.target as HTMLInputElement).value.split(
         /,\s*/
       );
 
-      // Render the forbidden words
       renderForbiddenWords(getForbiddenWordsText(text, forbiddenWords));
     });
 };
@@ -115,10 +86,8 @@ const listenFrequenciesChanges = (text: string): void => {
     .querySelectorAll("input")
     .forEach((frequencyInput, index) => {
       frequencyInput.addEventListener("input", (event) => {
-        // The variable word contains the text entered by the user in the input
         const word = (event.target as HTMLInputElement).value;
 
-        // Implement the frequency calculation here and delete this 0
         const frequency = getWordFrequency(text, word);
 
         frequenciesContainer.querySelector(
@@ -129,11 +98,9 @@ const listenFrequenciesChanges = (text: string): void => {
 };
 
 export const analyzeText = (text: string): void => {
-  // Keep this two calls
   listenFrequenciesChanges(text);
   listenForbiddenWordsChanges(text);
 
-  // Implement the rest of the analysis here
   renderParagraphsTotal(getParagraphsTotal(text));
   renderWordsTotal(getWordsTotal(text));
   renderCharactersTotal(getCharactersTotal(text));
